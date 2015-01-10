80b012dc08e1ea4db376bb0aef0dd50859e936ae
ali abrar <aliabrar@gmail.com>
2015-01-10 18:45:50 -0500

    Update Listen

--- js/src/Focus/JS/Listen.hs
+++ js/src/Focus/JS/Listen.hs
@@ -2,11 +2,12 @@ module Focus.JS.Listen where
 
 import Prelude hiding (foldl1)
 
+import Control.Monad.Fix
 import Data.List.NonEmpty (nonEmpty)
 import Data.Semigroup
 import Data.Foldable
-import Reflex.Class
+import Reflex
 import Data.Maybe
 
-accumNotifications :: MonadHoldEvent t m => (a -> b -> b) -> b -> (n -> Maybe a) -> Event t [n] -> m (Dynamic t b)
-accumNotifications g z f e = foldDyn (foldl1 (.) . fmap g) z =<< mapMaybeE (nonEmpty . catMaybes . map f) e
+accumNotifications :: (Control.Monad.Fix.MonadFix m, Reflex t, MonadHold t m) => (a -> b -> b) -> b -> (n -> Maybe a) -> Event t [n] -> m (Dynamic t b)
+accumNotifications g z f e = foldDyn (foldl1 (.) . fmap g) z $ fmapMaybe (nonEmpty . catMaybes . map f) e
