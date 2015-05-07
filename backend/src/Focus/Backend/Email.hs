ea504d92b9e95f0ebb8cc233c012e56a9cf2162c
Ali Abrar <aliabrar@gmail.com>
2015-05-07 18:18:37 -0400

    rename MonadEmail to Email

--- backend/src/Focus/Backend/Email.hs
+++ backend/src/Focus/Backend/Email.hs
@@ -1,11 +1,16 @@
 {-# LANGUAGE TemplateHaskell, FlexibleInstances, GeneralizedNewtypeDeriving, ScopedTypeVariables, MultiParamTypeClasses, TypeFamilies, FlexibleContexts, UndecidableInstances, OverloadedStrings #-}
-module Focus.Backend.MonadEmail where
+module Focus.Backend.Email where
 
 import Focus.Brand
 import Focus.Sign
 import Focus.Route
 import Focus.Backend.TH
-import Network.Mail.Mime (Mail (..))
+import Network.Mail.Mime (Mail (..), htmlPart)
+import Data.Text (Text)
+import Text.Blaze.Html5 (Html)
+import Data.List.NonEmpty (NonEmpty)
+import Data.Foldable
+import Text.Blaze.Html.Renderer.Text
 import Network.Mail.SMTP (sendMailWithLogin', simpleMail, Address (..))
 import Control.Monad.IO.Class
 import Control.Monad
@@ -30,5 +35,8 @@ instance MonadEmail m => MonadEmail (ReaderT r m) where
   sendMail = lift . sendMail
 
 runEmailT = runReaderT . unEmailT
+
+sendEmailFrom :: MonadEmail m => Text -> Text -> NonEmpty Address -> Text -> Html -> m ()
+sendEmailFrom name email recipients subject body = sendMail $ simpleMail (Address (Just name) email) (toList recipients) [] [] subject [(htmlPart $ renderHtml body)]
 
 deriveNewtypePersistBackend (\m -> [t| EmailT $m |]) (\m -> [t| ReaderT EmailEnv $m |]) 'EmailT 'unEmailT
