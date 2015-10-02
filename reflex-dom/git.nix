90334aba1a662f83569584d7461dc40423e9289c
Doug Beardsley <mightybyte@gmail.com>
2015-10-02 14:34:55 -0400

    Bump reflex-dom

--- reflex-dom/git.nix
+++ reflex-dom/git.nix
@@ -1,5 +1,5 @@
 {
   url = git://github.com/ryantrinkle/reflex-dom;
-  rev = "00d8ccf01a459d998223e07253cb881795b1643c";
-  sha256 = "595a1ce6e30a624b5335951d3b45acc339e196050ad200e6fa75ed8c9a1cd366";
+  rev = "3c8811113ee006b2e31665a8011fd104845e7b32";
+  sha256 = "0ivf2qak7mn6bppln19l8rrxy81pqmj0w16x8xjq769sq9k3mgf3";
 }
