f1ae90101744daa48af0494dffe243d14c1ed71c
Ali Abrar <aliabrar@gmail.com>
2015-10-07 16:48:00 -0400

    add resizeDetector to reflex-dom

--- reflex-dom/git.nix
+++ reflex-dom/git.nix
@@ -1,5 +1,5 @@
 {
   url = git://github.com/ryantrinkle/reflex-dom;
-  rev = "3c8811113ee006b2e31665a8011fd104845e7b32";
-  sha256 = "0ivf2qak7mn6bppln19l8rrxy81pqmj0w16x8xjq769sq9k3mgf3";
+  rev = "fb5eebfbd28f51f6a0403b71ae9f8af370532b65";
+  sha256 = "6c9be59b2c0560f848d9fe3dbc1bd76be044d4dfc2f930a23f8292298690bf8c";
 }
