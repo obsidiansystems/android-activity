ea502ad8e3b4925fdfaf7b77273583d8d83d12fe
Ali Abrar <aliabrar@gmail.com>
2015-10-07 17:21:42 -0400

    move back to reflex-dom develop branch

--- reflex-dom/git.nix
+++ reflex-dom/git.nix
@@ -1,5 +1,5 @@
 {
   url = git://github.com/ryantrinkle/reflex-dom;
-  rev = "fb5eebfbd28f51f6a0403b71ae9f8af370532b65";
-  sha256 = "6c9be59b2c0560f848d9fe3dbc1bd76be044d4dfc2f930a23f8292298690bf8c";
+  rev = "7dc0a76fbf910b917abe34c97f62afa51b2a46f7";
+  sha256 = "3a9862c71f15bbc54af22806c962bd5bbd4b8e602b79f3e3a22e795ece19fc80";
 }
