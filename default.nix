50468bc20dea6a6ec3cfb8c4e7c56ae00b17ab65
Ali Abrar <aliabrar@gmail.com>
2015-12-01 18:58:54 -0500

    add th to default.nix

--- default.nix
+++ default.nix
@@ -75,7 +75,7 @@ in rec {
         cabal-version: >= 1.2
         ${if executableName != null then executableHeader executableName else libraryHeader}
           hs-source-dirs: src
-          build-depends: ${pkgs.lib.concatStringsSep "," ([ "base" "bytestring" "containers" "time" "transformers" "text" "lens" "aeson" "mtl" "directory" "deepseq" "binary" "async" "vector" ] ++ (if haskellPackages.ghc.isGhcjs or false then [ "ghcjs-base" ] else []) ++ builtins.filter (x: x != null) (builtins.map (x: x.pname or null) depends))}
+          build-depends: ${pkgs.lib.concatStringsSep "," ([ "base" "bytestring" "containers" "time" "transformers" "text" "lens" "aeson" "mtl" "directory" "deepseq" "binary" "async" "vector" "template-haskell" ] ++ (if haskellPackages.ghc.isGhcjs or false then [ "ghcjs-base" ] else []) ++ builtins.filter (x: x != null) (builtins.map (x: x.pname or null) depends))}
           other-extensions: TemplateHaskell
           ghc-options: -threaded -Wall -fwarn-tabs -funbox-strict-fields -O2 -fprof-auto-calls -rtsopts -threaded -with-rtsopts=-N10
         EOF
