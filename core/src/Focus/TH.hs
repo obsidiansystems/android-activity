389351017ecc95aed69b7244f0bff5a20ffba77f
Cale Gibbard <cgibbard@gmail.com>
2016-02-02 14:37:00 -0500

    add embedFileString

--- core/src/Focus/TH.hs
+++ core/src/Focus/TH.hs
@@ -1,8 +1,11 @@
+{-# LANGUAGE TemplateHaskell #-}
 module Focus.TH where
 
 import Language.Haskell.TH
 import qualified Data.FileEmbed as FE
 import System.FilePath
+import qualified Data.Text as T
+import Data.Text.Encoding
 
 conName :: Con -> Name
 conName x = case x of
@@ -15,3 +18,7 @@ embedFile :: FilePath -> Q Exp
 embedFile p = do
   l <- location
   FE.embedFile $ takeDirectory (loc_filename l) </> p
+
+embedFileString :: FilePath -> Q Exp
+embedFileString fp = appE [| T.unpack . decodeUtf8 |] (embedFile fp)
+
