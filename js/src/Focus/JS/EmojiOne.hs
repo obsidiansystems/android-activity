41501fc83deaa81760925002371c1eb0f86f878a
Daniel Haraj <daniel.haraj@skedge.me>
2016-04-13 14:00:38 -0400

    stub out emojiPicker

--- js/src/Focus/JS/EmojiOne.hs
+++ js/src/Focus/JS/EmojiOne.hs
@@ -1,5 +1,12 @@
 {-# LANGUAGE TemplateHaskell #-}
-module Focus.JS.EmojiOne ( module Focus.EmojiOne
+module Focus.JS.EmojiOne ( module Focus.JS.EmojiOne
+                         , module Focus.EmojiOne
                          ) where
 
+import Reflex
+import Reflex.Dom
+
 import Focus.EmojiOne
+
+emojiPicker :: MonadWidget t m => m (Event t String)
+emojiPicker = return never
