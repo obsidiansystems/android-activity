5b9aa58d77031961348547f058eec7f4cfec760b
Cale Gibbard <cgibbard@gmail.com>
2016-04-18 16:12:48 -0400

    Refocus textarea when setting position of caret.

--- js/src/Focus/JS/TextArea.hs
+++ js/src/Focus/JS/TextArea.hs
@@ -4,6 +4,7 @@ module Focus.JS.TextArea where
 import Reflex.Dom
 import GHCJS.DOM.HTMLTextAreaElement (HTMLTextAreaElement)
 import qualified GHCJS.DOM.HTMLTextAreaElement as TA
+import qualified GHCJS.DOM.Element as El
 
 textAreaGetEnter :: Reflex t => TextArea t -> Event t ()
 textAreaGetEnter i = fmapMaybe (\n -> if n == keycodeEnter then Just () else Nothing) $ _textArea_keypress i
@@ -12,5 +13,5 @@ attachSelectionStart :: (HasJS x (WidgetHost m), MonadWidget t m) => HTMLTextAre
 attachSelectionStart t e = performEvent (fmap (\v -> do n <- TA.getSelectionStart t; return (n, v)) e)
 
 setSelectionPos :: (HasJS x (WidgetHost m), MonadWidget t m) => HTMLTextAreaElement -> Event t Int -> m ()
-setSelectionPos t e = performEvent_ (fmap (\n -> do TA.setSelectionStart t n; TA.setSelectionEnd t n) e)
+setSelectionPos t e = performEvent_ (fmap (\n -> do TA.setSelectionStart t n; TA.setSelectionEnd t n; El.focus t) e)
 
