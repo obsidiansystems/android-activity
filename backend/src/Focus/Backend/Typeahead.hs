9e8a3b5b7cfe39eb190435acfd99c4e15bbd60f7
Ali Abrar <aliabrar@gmail.com>
2016-04-21 15:54:36 -0400

    Add Focus.Patch and Focus.Backend.Typeahead

---
+++ backend/src/Focus/Backend/Typeahead.hs
@@ -0,0 +1,66 @@
+module Focus.Backend.Typeahead where
+
+import Control.Lens
+import Data.Map (Map)
+import qualified Data.Map as Map
+import Data.Monoid
+import Data.Set (Set)
+import qualified Data.Set as Set
+import qualified Data.Text as T
+
+import Focus.Backend.Listen
+
+typeaheadPatch :: (Ord k, Monoid a1)
+               => NotificationType
+               -> k -- Account Id
+               -> t -- Account
+               -> Set T.Text -- Queries
+               -> ASetter a1 a a2 (Map T.Text (Maybe (Set k, Set k))) -- results lens
+               -> ASetter a b a3 (Map k (Maybe t)) -- view lens
+               -> (t -> Maybe T.Text) -- to text
+               -> b -- patch
+typeaheadPatch nt aid acc qs rlens vlens toText = 
+  let results = Map.mapMaybe id $ Map.fromSet (\q -> isPrefix nt q aid acc toText) qs 
+  in mempty & rlens .~ results
+            & vlens .~ mapIntersectionWithSetPatchMap (Map.singleton aid (ntPatch nt acc)) results
+  where
+    mapIntersectionWithSetPatchMap :: (Ord k, Ord k') => Map k v -> Map k' (Maybe (Set k, Set k)) -> Map k v
+    mapIntersectionWithSetPatchMap m spm = Map.intersection m $ Map.foldl (\xs mSetPatch ->
+        case mSetPatch of
+             Just (a, r) -> Map.fromSet (const ()) a <> Map.fromSet (const ()) r <> xs
+             _ -> xs) Map.empty spm
+
+typeaheadPatch' :: (Ord a, Monoid a2)
+                => NotificationType
+                -> a -- Account Id
+                -> t -- Account
+                -> Set T.Text -- Queries
+                -> ASetter a2 a1 a3 (Map T.Text (Maybe (Set a, Set a))) -- Results lens
+                -> ASetter a1 b a4 b1 -- view lens
+                -> (t -> Maybe T.Text) -- to text
+                -> (Map T.Text (Maybe (Set a, Set a)) -> b1) -- to view
+                -> b -- patch
+typeaheadPatch' nt aid acc qs rlens vlens toText f = let results = Map.mapMaybe id $ Map.fromSet (\q -> isPrefix nt q aid acc toText) qs in mempty
+            & rlens .~ results
+            & vlens .~ f results
+
+isPrefix :: Ord a
+         => NotificationType
+         -> T.Text
+         -> a
+         -> t
+         -> (t -> Maybe T.Text)
+         -> Maybe (Maybe (Set a, Set a))
+isPrefix nt q aid acc f = case fmap (q `T.isPrefixOf`) (f acc) of
+  Just True -> Just $ Just $ ntSetPatch nt aid
+  _ -> Nothing
+
+ntPatch :: NotificationType -> a -> Maybe a
+ntPatch nt' = case nt' of
+  Delete -> const Nothing
+  _ -> Just
+
+ntSetPatch :: Ord a => NotificationType -> a -> (Set a, Set a)
+ntSetPatch nt' = case nt' of
+  Delete -> \x -> (mempty, Set.singleton x)
+  _ -> \x -> (Set.singleton x, mempty)
---
+++ core/src/Focus/Patch.hs
@@ -0,0 +1,35 @@
+{-# LANGUAGE TemplateHaskell, GeneralizedNewtypeDeriving, EmptyDataDecls, MultiParamTypeClasses, TypeFamilies, DeriveGeneric, FlexibleInstances, FunctionalDependencies #-}
+module Focus.Patch where
+
+import Control.Monad
+import Data.Aeson
+import Data.Map (Map)
+import qualified Data.Map as Map
+import Data.Set (Set)
+import qualified Data.Set as Set
+import GHC.Generics
+
+import Focus.Schema ()
+
+patchMap :: Ord k => Map k (Maybe v) -> Map k v -> Map k v
+patchMap = Map.mergeWithKey (\_ p _ -> p) (Map.mapMaybe id) id
+
+patchMapWith :: Ord k => (p -> Maybe v) -> (p -> v -> v) -> Map k (Maybe p) -> Map k v -> Map k v
+patchMapWith i f = Map.mergeWithKey (\_ p v -> maybe (\_ -> Nothing) ((Just.) . f) p v) (Map.mapMaybe (join . fmap i)) id
+
+ -- TODO: Use a version of makeJson that can handle instance heads instead of aeson's generic deriving
+newtype SetPatch a = SetPatch (Map a Bool)
+  deriving (Show, Read, Eq, Ord, Generic, Monoid)
+
+instance (Ord a, ToJSON a) => ToJSON (SetPatch a)
+instance (Ord a, FromJSON a) => FromJSON (SetPatch a)
+
+applySetPatch :: Ord a => SetPatch a -> Set a -> Set a
+applySetPatch (SetPatch sp) set = Map.keysSet (Map.filter id (Map.union sp (Map.fromSet (\_ -> True) set)))
+
+mapIntersectionWithKeysSet :: Ord k => Map k v -> Set k -> Map k v
+mapIntersectionWithKeysSet m s = Map.intersection m $ Map.fromSet (const ()) s
+
+mapIntersectionWithSetMap :: (Ord k, Ord k') => Map k v -> Map k' (Set k) -> Map k v
+mapIntersectionWithSetMap m sm = mapIntersectionWithKeysSet m $ Set.unions $ Map.elems sm
+
