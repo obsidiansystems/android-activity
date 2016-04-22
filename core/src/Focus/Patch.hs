e548b623002d188b2579cc0bb2e424d9925e1c18
Ali Abrar <aliabrar@gmail.com>
2016-04-21 20:58:55 -0400

    Add typeahead patching functions

--- core/src/Focus/Patch.hs
+++ core/src/Focus/Patch.hs
@@ -33,3 +33,17 @@ mapIntersectionWithKeysSet m s = Map.intersection m $ Map.fromSet (const ()) s
 mapIntersectionWithSetMap :: (Ord k, Ord k') => Map k v -> Map k' (Set k) -> Map k v
 mapIntersectionWithSetMap m sm = mapIntersectionWithKeysSet m $ Set.unions $ Map.elems sm
 
+applyTypeaheadPatch :: (Ord k, Ord a) => Map k (Maybe (Set a, Set a)) -> Map k (Set a) -> Map k (Set a)
+applyTypeaheadPatch resultsPatch results = Map.mergeWithKey (\_ p v -> mergeTypeaheadResults p v) (Map.map fst . Map.mapMaybe id) id resultsPatch results
+  where
+    mergeTypeaheadResults p v = case p of
+      Nothing -> Nothing
+      Just (adds, removes) -> Just $ Set.union adds $ Set.difference v removes
+
+intersectTypeaheadResults :: (Ord k, Ord k')
+                          => Map k v -- All values
+                          -> Set k' -- Queries in view selector
+                          -> Map k' (Set k) -- Map of query results
+                          -> Map k v -- Filtered values
+intersectTypeaheadResults vall vsq vqr = Map.intersection vall . Map.fromSet (const ()) . Set.unions . Map.elems . Map.intersection vqr $ Map.fromSet (const ()) vsq
+
