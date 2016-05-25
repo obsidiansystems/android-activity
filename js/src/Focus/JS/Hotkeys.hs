280a239f721fecda288a814ab1dcb1b5340b97bc
Ali Abrar <aliabrar@gmail.com>
2016-05-25 17:14:30 -0400

    Add inputTarget to determine which events targeted input elements

--- js/src/Focus/JS/Hotkeys.hs
+++ js/src/Focus/JS/Hotkeys.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE LambdaCase, ScopedTypeVariables #-}
 module Focus.JS.Hotkeys where
 
 import Control.Monad.IO.Class
@@ -7,8 +7,10 @@ import Data.Map (Map)
 import qualified Data.Map as Map
 import Data.Maybe
 import Data.Set (Set)
-import GHCJS.DOM.Types (IsElement)
+import GHCJS.DOM.Types (IsElement, KeyboardEvent)
+import qualified GHCJS.DOM.Element as E
 import qualified GHCJS.DOM.EventM as E
+import qualified GHCJS.DOM.HTMLElement as HE
 import qualified GHCJS.DOM.KeyboardEvent as E
 import Reflex.Dom
 
@@ -19,11 +21,12 @@ data KeyModifier = KeyModifier_Alt
                  | KeyModifier_AltGr
   deriving (Show, Read, Eq, Ord, Enum, Bounded)
 
+-- | Bind keycodes + modifiers
 bindKeys :: (MonadIO m, MonadWidget t m, IsElement e)
-         => E.EventName e E.KeyboardEvent
-         -> e
-         -> Map (Int, Set KeyModifier) (ReaderT E.KeyboardEvent IO k)
-         -> m (Event t k)
+         => E.EventName e E.KeyboardEvent -- ^ The keyboard event name (e.g., keyDown)
+         -> e -- ^ The element to listen on
+         -> Map (Int, Set KeyModifier) (ReaderT  E.KeyboardEvent IO k) -- ^ Map of key combinations to actions
+         -> m (Event t k) -- ^ Event that fires when any of the specified key combinations is pressed
 bindKeys keyEv element hotkeys = do
   fmap (fmapMaybe id) $ wrapDomEvent element (`E.on` keyEv) $ do
     e <- E.event
@@ -39,3 +42,9 @@ bindKeys keyEv element hotkeys = do
           Map.filterWithKey (\(kc, reqMods) _ -> keyEvent == kc && reqMods == activeMods) hotkeys
     sequence activeHotkey
 
+-- | Determine whether the event target is an element that takes input
+inputTarget :: ReaderT KeyboardEvent IO Bool
+inputTarget = do
+  Just e <- fmap (fmap HE.castToHTMLElement) E.eventTarget
+  n :: Maybe String <- E.getTagName e
+  return $ n `elem` (map Just ["INPUT", "SELECT", "TEXTAREA"])
