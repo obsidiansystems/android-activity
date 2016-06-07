3269f8c29c12e52ffb91e4da687480130a618994
Ali Abrar <aliabrar@gmail.com>
2016-06-07 13:47:12 -0400

    Add Focus.GoogleAnalytics

---
+++ core/src/Focus/GoogleAnalytics.hs
@@ -0,0 +1,16 @@
+{-# LANGUAGE TemplateHaskell #-}
+module Focus.GoogleAnalytics where
+
+import Data.Monoid ((<>))
+import Data.Text (Text)
+import Control.Lens (makeLenses)
+import Focus.Request (makeJson)
+
+data GoogleAnalyticsEnv = GoogleAnalyticsEnv { _googleAnalyticsEnv_trackingId :: Text }
+
+googleAnalyticsScript :: GoogleAnalyticsEnv -> Text
+googleAnalyticsScript env = "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){ (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o), m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m) })(window,document,'script','https://www.google-analytics.com/analytics.js','ga'); ga('create', '" <> (_googleAnalyticsEnv_trackingId env) <> "', 'auto'); ga('send', 'pageview');"
+
+makeJson ''GoogleAnalyticsEnv
+makeLenses ''GoogleAnalyticsEnv
+
