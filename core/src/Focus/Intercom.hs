58561c8f3e81bec813adf4cffbb5f7ea12a2e227
Luigy Leon <luigy@outlook.com>
2016-07-06 17:03:53 -0400

    implement retrieving users from intercom

--- core/src/Focus/Intercom.hs
+++ core/src/Focus/Intercom.hs
@@ -1,18 +1,22 @@
-{-# LANGUAGE TemplateHaskell, OverloadedStrings, GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE RecordWildCards, TemplateHaskell, OverloadedStrings, GeneralizedNewtypeDeriving #-}
 module Focus.Intercom where
 
+import Control.Lens (makeLenses)
+import Data.Aeson
+import Data.Aeson.Types
+import qualified Data.ByteString.Lazy as LBS
+import Data.Digest.Pure.SHA (showDigest, hmacSha256)
 import Data.Monoid ((<>))
 import Data.Text (Text)
 import qualified Data.Text as T
-import Data.Text.Encoding (encodeUtf8)
-import Control.Lens (makeLenses)
-import Data.Aeson
-import Focus.Request (makeJson)
-import qualified Data.ByteString.Lazy as LBS
-
-import Data.Digest.Pure.SHA (showDigest, hmacSha256)
+import qualified Data.Text.Encoding as T
+import Data.Time
+import Data.Time.Clock.POSIX
+import Network.HTTP.Conduit
 
 import Focus.Intercom.Common (IntercomUserHash(..))
+import Focus.Request (makeJson)
+
 
 newtype IntercomSecretKey = IntercomSecretKey { unIntercomSecretKey :: Text } deriving (Show, Read, Eq, Ord, FromJSON, ToJSON)
 
@@ -30,4 +34,39 @@ intercomScript env = "(function(){var w=window;var ic=w.Intercom;w.intercom_app_
 -- Hash an account id with the intercom secret key
 -- NOTE: We use email. But support for account id can also be added
 hashIntercom :: IntercomSecretKey -> Text -> IntercomUserHash
-hashIntercom (IntercomSecretKey intercomSecretKey) email = IntercomUserHash $ T.pack $ showDigest $ hmacSha256 (LBS.fromStrict $ encodeUtf8 intercomSecretKey) $ LBS.fromStrict $ encodeUtf8 email
+hashIntercom (IntercomSecretKey intercomSecretKey) email = IntercomUserHash $ T.pack $ showDigest $ hmacSha256 (LBS.fromStrict $ T.encodeUtf8 intercomSecretKey) $ LBS.fromStrict $ T.encodeUtf8 email
+
+data IntercomUser = IntercomUser
+       { _intercomUser_updatedAt :: UTCTime
+       , _intercomUser_id :: Text
+       , _intercomUser_email :: Text
+       }
+  deriving (Show, Read, Eq, Ord)
+instance FromJSON IntercomUser where
+  parseJSON = withObject "IntercomUser" $ \o -> do
+    _intercomUser_updatedAt <- fmap posixSecondsToUTCTime (o .: "updated_at")
+    _intercomUser_id <- o .: "id"
+    _intercomUser_email <- o .: "email"
+    return IntercomUser {..}
+
+getIntercomUsers :: IntercomEnv -> IO [IntercomUser]
+getIntercomUsers env = do
+  let url = "https://api.intercom.io/users"
+  res <- getReq env url
+  return $ maybe mempty id $ parseMaybe userListParser =<< decode res
+  where
+    userListParser = withObject "getIntercomUsers" (.: "users")
+
+    getReq env' url = do
+      man <- newManager tlsManagerSettings
+      req <- fmap (asJson . applyBasicAuth user pass) $ parseUrl url
+      res <- httpLbs req man
+      return $ responseBody res
+      where
+        user = T.encodeUtf8 $ _intercomEnv_appId env'
+        pass = T.encodeUtf8 $ unIntercomSecretKey $ _intercomEnv_secretKey env'
+        asJson req = req { requestHeaders = ("Accept", "application/json"):requestHeaders req }
+
+conversationsUrlFromId :: Text -> Text -> Text
+conversationsUrlFromId appId userId =
+  "https://app.intercom.io/a/apps/" <> appId <> "/users/" <> userId <> "/all-conversations"
