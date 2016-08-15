3822e57ff16a7f7fae2b17b9dad933f6a829ab00
Ryan Trinkle <ryan.trinkle@gmail.com>
2016-08-15 14:54:52 -0400

    Refactor intercom support

--- js/src/Focus/JS/Intercom.hs
+++ js/src/Focus/JS/Intercom.hs
@@ -1,28 +1,31 @@
 {-# LANGUAGE ForeignFunctionInterface, JavaScriptFFI, CPP, TemplateHaskell, NoMonomorphismRestriction, EmptyDataDecls, RankNTypes, GADTs, RecursiveDo, ScopedTypeVariables, FlexibleInstances, MultiParamTypeClasses, TypeFamilies, FlexibleContexts, DeriveDataTypeable, GeneralizedNewtypeDeriving, StandaloneDeriving, ConstraintKinds, UndecidableInstances #-}
 module Focus.JS.Intercom
-( IntercomUserHash(..)
-, IntercomUserSettings(..)
-, setupIntercom
-, shutdownIntercom
-) where
+       ( IntercomUserHash (..)
+       , IntercomVisitor (..)
+       , IntercomUserSettings (..)
+       , intercom
+       , setupIntercom
+       , shutdownIntercom
+       ) where
 
-import Control.Monad hiding (forM, forM_, mapM, mapM_, sequence)
+import Control.Monad
+import Data.Text (Text)
 import qualified Data.Text as T
-
+import Focus.Intercom.Common
 import Focus.JS.App
 import Reflex
 import Reflex.Dom
 
-import Focus.Intercom.Common
+data IntercomVisitor
+  = IntercomVisitor_Lead -- ^ Leads are anonymous
+  | IntercomVisitor_User IntercomUserSettings -- ^ Users are known to the system and authenticated
 
-data IntercomUserSettings
-  = AnonymousUser
-  | IntercomUserSettings
-    { intercomUserHash :: IntercomUserHash
-    , intercomEmail :: T.Text
-    -- Why is this String? Because accountType in Account is a String
-    , intercomAccountType :: String
-    } deriving (Read, Show)
+data IntercomUserSettings = IntercomUserSettings
+  { _intercomUserSettings_hash :: IntercomUserHash
+  , _intercomUserSettings_email :: Text
+  -- Why is this String? Because accountType in Account is a String
+  , _intercomUserSettings_accountType :: String
+  } deriving (Read, Show)
 
 
 -- intercom startup for logged in users
@@ -34,19 +37,35 @@ importJS Unsafe "(function(){var o = {}; o['app_id'] = window['intercom_app_id']
 -- intercom shutdown
 importJS Unsafe "(function() {console.log('shutdown'); window['Intercom']('shutdown');})['call'](this)" "intercomShutdown" [t| forall x m. MonadJS x m => m () |]
 
-setupIntercom :: (MonadFocusWidget app t m, HasJS x (Performable m)) => IntercomUserSettings -> Event t IntercomUserSettings -> m ()
-setupIntercom currentIntercomSettings intercomSettingsChangeEvt = do
+-- | Run Intercom and keep it updated with the visitor provided.  When 'Nothing'
+-- is provided, intercom will be shut down completely (i.e. not visible on the
+-- page).  To specify an anonymous user, use @Just IntercomVisitor_Lead@.
+--
+-- WARNING: You may only have one of these on the page at time; otherwise they
+-- will interfere with each other.
+intercom :: (PostBuild t m, PerformEvent t m, HasJS x (Performable m)) => Dynamic t (Maybe IntercomVisitor) -> m (Event t ())
+intercom visitor = do
   pb <- getPostBuild
-  performEvent_ $ fmap (\u -> liftJS intercomShutdown >> liftJS (mkUpdate u)) (mergeWith (flip const) [fmap (const currentIntercomSettings) pb, intercomSettingsChangeEvt])
+  let setIntercomSettings = leftmost
+        [ updated visitor
+        , tag (current visitor) pb
+        ]
+  performEvent $ ffor setIntercomSettings $ \s -> liftJS $ do
+    intercomShutdown
+    mapM_ mkUpdate s
   where
-    mkUpdate AnonymousUser = intercomStartupAnon
-    mkUpdate intercomUserSettings@IntercomUserSettings{} = intercomStartup email 0 email iuh accountType
+    mkUpdate IntercomVisitor_Lead = intercomStartupAnon
+    mkUpdate (IntercomVisitor_User intercomUserSettings) = intercomStartup email 0 email iuh accountType
       where
-        iuh = T.unpack $ unIntercomUserHash $ intercomUserHash intercomUserSettings
-        email = T.unpack $ intercomEmail intercomUserSettings
-        accountType = intercomAccountType intercomUserSettings
+        iuh = T.unpack $ unIntercomUserHash $ _intercomUserSettings_hash intercomUserSettings
+        email = T.unpack $ _intercomUserSettings_email intercomUserSettings
+        accountType = _intercomUserSettings_accountType intercomUserSettings
 
-shutdownIntercom :: (MonadFocusWidget app t m, HasJS x (Performable m)) => m (Event t ())
-shutdownIntercom = do
-  pb <- getPostBuild
-  performEvent <=< headE $ ffor pb $ \_ -> liftJS intercomShutdown
+-- TODO: Make this automatic when the 'intercom' widget disappears
+{-# DEPRECATED shutdownIntercom "Use 'intercom (constDyn Nothing)' instead" #-}
+shutdownIntercom :: (PostBuild t m, PerformEvent t m, HasJS x (Performable m)) => m (Event t ())
+shutdownIntercom = intercom $ constDyn Nothing
+
+{-# DEPRECATED setupIntercom "Use 'void . intercom . fmap Just =<< holdDyn v0 newV' instead of 'setupIntercom v0 newV'" #-}
+setupIntercom :: (MonadFocusWidget app t m, HasJS x (Performable m)) => IntercomVisitor -> Event t IntercomVisitor -> m ()
+setupIntercom v0 v' = void . intercom . fmap Just =<< holdDyn v0 v'
