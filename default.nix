ee028537e73e0134d08195796c9d7e9c719aea15
Ali Abrar <aliabrar@gmail.com>
2016-12-02 14:59:16 -0500

    Add email-parse to focus

--- default.nix
+++ default.nix
@@ -50,6 +50,7 @@ rec {
              });
              focus-serve = dontHaddock (self.callPackage (cabal2nixResult (filterGitSource ./http/serve)) {});
              focus-th = dontHaddock (self.callPackage (cabal2nixResult (filterGitSource ./th)) {});
+             email-parse = dontHaddock (self.callPackage (cabal2nixResult (filterGitSource ./email-parse)) {});
            };
       extendFrontendHaskellPackages = haskellPackages: (haskellPackages.override {
         overrides = self: super: sharedOverrides self super // {
