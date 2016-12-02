8b8d9bf0ed16085c50e22cfd47645c3f5f9671af
Anupam Jain <ajnsit@gmail.com>
2016-12-02 23:58:26 +0530

    1. Use the scroll API to fetch intercom users. 2. Make some fields optional. 3. Move IntercomUser data structure to Common and decouple it from the API so it can be used by app specific code.

--- core/src/Focus/Intercom/Common.hs
+++ core/src/Focus/Intercom/Common.hs
@@ -3,6 +3,20 @@ module Focus.Intercom.Common where
 
 import Data.Text (Text)
 import Data.Aeson
+import Data.Time (UTCTime)
+import Data.Int (Int64)
 
--- COMMON
 newtype IntercomUserHash = IntercomUserHash { unIntercomUserHash :: Text } deriving (Read, Show, Eq, Ord, ToJSON, FromJSON)
+
+data IntercomUser = IntercomUser
+       { _intercomUser_updatedAt :: UTCTime
+       , _intercomUser_txtId :: Text
+       -- ^ This is not named _id so it doesn't clash with groundhog's auto generated key
+       , _intercomUser_email :: Text
+       , _intercomUser_sessionCount :: Int64
+       , _intercomUser_location :: Text
+       , _intercomUser_userAgent :: Text
+       , _intercomUser_device :: Text
+       , _intercomUser_browser :: Text
+       }
+  deriving (Show, Read, Eq, Ord)
