7e769d211cf8951e8c80ce4fb3449ef455322e95
Daniel Haraj <dan@obsidian.systems>
2016-12-31 20:33:11 -0500

    Add PrimitivePersistField instance for Json wrapper

--- backend/src/Focus/Backend/Schema.hs
+++ backend/src/Focus/Backend/Schema.hs
@@ -4,6 +4,7 @@ module Focus.Backend.Schema where
 import Data.Aeson
 import Database.Groundhog.Core
 import Database.Groundhog.Generic.Sql
+import Data.Functor.Identity
 
 import Focus.Schema (Json (..))
 
@@ -16,3 +17,9 @@ instance (ToJSON a, FromJSON a) => PersistField (Json a) where
     Just r' <- return $ decode' r
     return (Json r', vs)
   dbType p (Json a) = dbType p (encode a)
+
+instance (ToJSON a, FromJSON a) => PrimitivePersistField (Json a) where
+  toPrimitivePersistValue p (Json a) = toPrimitivePersistValue p (encode a)
+  fromPrimitivePersistValue p v = runIdentity $ do
+    Just r <- return $ decode' $ fromPrimitivePersistValue p v
+    return (Json r)
