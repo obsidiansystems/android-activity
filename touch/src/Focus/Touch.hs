3d378e39944489301f8cda3dd82609eecda8456a
Ali Abrar <aliabrar@gmail.com>
2017-02-04 23:44:17 -0500

    Add Gesture_Drag to Focus.Touch

--- touch/src/Focus/Touch.hs
+++ touch/src/Focus/Touch.hs
@@ -6,7 +6,6 @@
 module Focus.Touch where
 import Control.Monad.IO.Class
 import Data.Align
-import Data.Bifunctor (bimap)
 import Data.Default
 import Data.List.NonEmpty (NonEmpty (..))
 import qualified Data.List.NonEmpty as NonEmpty
@@ -79,35 +78,39 @@ resolveTouchEvents :: TouchConfig
                    -> TouchEventStage
                    -> Map Word UnfinishedGesture
                    -> (Map Word UnfinishedGesture, [Gesture])
-resolveTouchEvents cfg te state =
-  bimap (Map.mapMaybe id) Map.elems $ Map.mapEither id $ case te of
-    TouchEventStage_Start t r -> fmap (Left . Just) . Map.union state $
-      UnfinishedGesture t <$> changedTouches r
-    TouchEventStage_Move r -> touchAlign state r $ \(UnfinishedGesture t0 a) b ->
-      Left $ Just $ UnfinishedGesture t0 $ a <> b
-    TouchEventStage_Cancel r -> touchAlign state r $ \_ _ -> Left Nothing
-    TouchEventStage_End t1 r -> touchAlign state r $ \(UnfinishedGesture t0 a) end ->
-      let p1 = coords $ NonEmpty.head a
-          p2 = coords $ NonEmpty.last end
-          dist = distance p1 p2
-          deg = angle p1 p2
-          minDist = _touchConfig_minimumSwipeDistance cfg
-          longPress = _touchConfig_longPress cfg
-      in if | dist < minDist && diffUTCTime t1 t0 > longPress -> Right Gesture_LongPress
-            | dist < minDist -> Right Gesture_Tap
-            | otherwise -> Right $ Gesture_Swipe (diffUTCTime t1 t0) $ Swipe
-              { _swipe_distance = dist
-              , _swipe_direction = deg
-              }
+resolveTouchEvents cfg te state = case te of
+  TouchEventStage_Start t r -> (Map.union state (UnfinishedGesture t <$> changedTouches r), [])
+  TouchEventStage_Cancel r -> touchAlign state r $ \_ _ -> (Nothing, Nothing)
+  TouchEventStage_Move r -> touchAlign state r $ \(UnfinishedGesture t0 a) b ->
+    let p1 = coords $ NonEmpty.head a
+        p2 = coords $ NonEmpty.last b
+        dist = distance p1 p2
+        deg = angle p1 p2
+    in (Just $ UnfinishedGesture t0 $ a <> b, Just $ Gesture_Drag $ Swipe { _swipe_direction = deg, _swipe_distance = dist })
+  TouchEventStage_End t1 r -> touchAlign state r $ \(UnfinishedGesture t0 a) end ->
+    let p1 = coords $ NonEmpty.head a
+        p2 = coords $ NonEmpty.last end
+        dist = distance p1 p2
+        deg = angle p1 p2
+    in if | dist < minDist && diffUTCTime t1 t0 > longPress -> (Nothing, Just Gesture_LongPress)
+          | dist < minDist -> (Nothing, Just Gesture_Tap)
+          | otherwise -> (,) Nothing $ Just $ Gesture_Swipe (diffUTCTime t1 t0) $ Swipe
+            { _swipe_distance = dist
+            , _swipe_direction = deg
+            }
   where
     touchAlign :: Map Word UnfinishedGesture
                -> TouchEventResult
-               -> (UnfinishedGesture -> NonEmpty TouchResult -> Either (Maybe UnfinishedGesture) Gesture)
-               -> Map Word (Either (Maybe UnfinishedGesture) Gesture)
-    touchAlign s r f = (\x -> alignWith x s $ changedTouches r) $ \case
-      This a -> Left $ Just a -- Touches for this identifier have not been modified
-      These a b -> f a b
-      That _ -> Left Nothing -- Received a touch update for an unstarted touch (shouldn't happen)
+               -> (UnfinishedGesture -> NonEmpty TouchResult -> (Maybe UnfinishedGesture, Maybe Gesture))
+               -> (Map Word UnfinishedGesture, [Gesture])
+    touchAlign s r f =
+      let r' = (\x -> alignWith x s $ changedTouches r) $ \case
+                This a -> (Just a, Nothing) -- Touches for this identifier have not been modified
+                These a b -> f a b
+                That _ -> (Nothing, Nothing) -- Received a touch update for an unstarted touch (shouldn't happen)
+      in (Map.mapMaybe fst r', Map.elems $ Map.mapMaybe snd r')
+    minDist = _touchConfig_minimumSwipeDistance cfg
+    longPress = _touchConfig_longPress cfg
 
 distance :: Floating a => (a, a) -> (a, a) -> a
 distance (x1, y1) (x2, y2) = sqrt ((x2 - x1)^(2::Int) + (y2 - y1)^(2::Int))
