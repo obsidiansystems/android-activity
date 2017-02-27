aeda46488cf879a3d0d6015de8a852c90959a659
Dan Haraj <dan@obsidian.systems>
2017-02-27 17:58:50 -0500

    keep track of contemperaneous requests and responses coarsely

--- parse-backend-listen.hs
+++ parse-backend-listen.hs
@@ -25,21 +25,35 @@ main = do
   files <- getArgs
   forM_ files $ \file -> do
     contents <- T.fromStrict <$> TS.readFile file
-    let acc :: (Map UTCTime [Text], Map UTCTime [Text]) -> [Text] -> (Map UTCTime [Text], Map UTCTime [Text])
+    let acc :: (Map UTCTime ([Text], Int), Map UTCTime ([Text], Int)) -> [Text] -> (Map UTCTime ([Text], Int), Map UTCTime ([Text], Int))
         acc um@(unmatchedOpens, unmatchedCloses) (header:rawtime:rest)
-          | header == "RESPONSE" = case Map.lookup time unmatchedOpens of
-              Nothing -> (unmatchedOpens, Map.insert time rest unmatchedCloses)
-              Just _ -> (Map.delete time unmatchedOpens, unmatchedCloses)
-          | header == "REQUEST" = (Map.insert time rest unmatchedOpens, unmatchedCloses)
+          | Just time <- parseTime rawtime
+          , header == "RESPONSE" = case Map.lookup time unmatchedOpens of
+              Nothing -> case Map.lookup time unmatchedCloses of
+                Just (x, count) -> (unmatchedOpens, Map.insert time (x, count+1) unmatchedCloses)
+                Nothing -> (unmatchedOpens, Map.insert time (rest, 1) unmatchedCloses)
+              Just (x, count) | count == 1 -> (Map.delete time unmatchedOpens, unmatchedCloses)
+                              | count > 1 -> (Map.insert time (x, count-1) unmatchedOpens, unmatchedCloses)
+                              | otherwise -> error "request count invariant failed"
+          | Just time <- parseTime rawtime
+          , header == "REQUEST" = case Map.lookup time unmatchedOpens of
+              Nothing -> (Map.insert time (rest, 1) unmatchedOpens, unmatchedCloses)
+              Just (x, count) -> (Map.insert time (x, count+1) unmatchedOpens, unmatchedCloses)
           | otherwise = um
-         where Just time = parseTimeM False defaultTimeLocale tsFormat (T.unpack rawtime)
-        acc acc _ = acc
-        pipeline :: Text -> (Map UTCTime [Text], Map UTCTime [Text])
+         where parseTime = parseTimeM False defaultTimeLocale tsFormat . T.unpack
+        acc um _ = um
+        pipeline :: Text -> (Map UTCTime ([Text], Int), Map UTCTime ([Text], Int))
         pipeline = foldl' acc (Map.empty, Map.empty) . map (T.splitOn "\t") . T.lines
         (unmatchedRequests, unmatchedResponses) = pipeline contents
     T.putStrLn "### Unmatched Requests"
-    forM_ (Map.toList unmatchedRequests) $ \(k, x) -> T.putStrLn . T.intercalate "\t" $
-      ["REQUEST", T.pack (formatTime defaultTimeLocale tsFormat k)] ++ x
+    forM_ (Map.toList unmatchedRequests) $ \(k, (x,count)) -> do
+      T.putStrLn . T.intercalate "\t" $
+        ["REQUEST", T.pack (formatTime defaultTimeLocale tsFormat k)] ++ x
+      when (count > 1) $ putStrLn $ mconcat $
+        ["\t... and ", show (count - 1), " unmatched request(s) at the same time."]
     T.putStrLn "### Unmatched Responses"
-    forM_ (Map.toList unmatchedResponses) $ \(k, x) -> T.putStrLn . T.intercalate "\t" $
-      ["RESPONSE", T.pack (formatTime defaultTimeLocale tsFormat k)] ++ x
+    forM_ (Map.toList unmatchedResponses) $ \(k, (x, count)) -> do
+      T.putStrLn . T.intercalate "\t" $
+        ["RESPONSE", T.pack (formatTime defaultTimeLocale tsFormat k)] ++ x
+      when (count > 1) $ putStrLn $ mconcat $
+        ["\t... and ", show (count - 1), " unmatched response(s) at the same time."]
