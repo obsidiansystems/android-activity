c5398dc9059c699fcc68ecaf4e56a92a83c29237
Ali Abrar <aliabrar@gmail.com>
2017-05-10 20:45:25 -0400

    Allow email server to skip failing jobs

--- backend/src/Focus/Backend/QueueWorker.hs
+++ backend/src/Focus/Backend/QueueWorker.hs
@@ -3,16 +3,12 @@ module Focus.Backend.QueueWorker where
 import Control.Concurrent
 import Control.Monad
 import Control.Monad.IO.Class
-import Data.Pool
-import Database.Groundhog.Postgresql
 
-import Focus.Backend.DB
 import Focus.Concurrent
 
-worker :: (MonadIO m, RunDb f)
+worker :: MonadIO m
        => Int -- ^ Thread delay
-       -> f (Pool Postgresql)
-       -> FocusPersist ()
+       -> IO ()
        -> m (IO ()) -- ^ Terminate action
-worker delay db f = return . killThread <=< liftIO . forkIO . supervise .  void . forever $
-  runDb db f >> threadDelay delay
+worker delay f = return . killThread <=< liftIO . forkIO . supervise .  void . forever $
+  f >> threadDelay delay
