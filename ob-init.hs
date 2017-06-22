4328bcfda3c41fd9d8e3ab1297758732334ff818
hsloan <ishaqsloan@gmail.com>
2017-06-22 17:59:53 -0400

    tidy code ob-init.hs, added ob-init-default.nix in hopes to embed the file contents into a default.nix file during script execution.

--- ob-init.hs
+++ ob-init.hs
@@ -1,14 +1,12 @@
 #!/usr/bin/env runhaskell
---TODO: Don't depend on the user's environment - everything should be based on nixpkgs via reflex-platform
-{-
-#Obsidian Systems - Reflex Focus
-#Author: Ishaq Sloan
-#Desc: This script prompts user for the project name, and
-# initializes the Reflex-Focus submodules along with appropriate
-# directory layout fit for best developer environment results.
-# This script is to be used within an existing
-# github repository and assumes that you have already
-# configured/added your SSH key with both github and gitlab.
+-- |TODO: Don't depend on the user's environment - everything should be based on nixpkgs via reflex-platform
+{-|
+ Description: This script prompts user for the project name, and
+ initializes the Reflex-Focus submodules along with appropriate
+ directory layout fit for best developer environment results.
+ This script is to be used within an existing
+ github repository and assumes that you have already
+ configured/added your SSH key with both github and gitlab.
 -}
 
 
@@ -16,21 +14,20 @@
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE TemplateHaskell #-}
 
-import System.Process
-import System.Directory
-import qualified Data.Text as Text
+import System.Process (callProcess)
+import System.Directory ( doesDirectoryExist
+                        , createDirectoryIfMissing
+                        , doesFileExist)
 import Data.Text (Text)
-import Data.ByteString as BS
-import Data.ByteString (ByteString)
-import Control.Monad
+import Data.FileEmbed (embedStringFile)
 
 main :: IO ()
 main = do
   --TODO consider passing an arg instead of using a prompt
-  Prelude.putStr "Project name: "
-  projectName <- BS.getLine
+  putStr "Project name: "
+  projectName <- getLine
   doesDirectoryExist "focus" >>= \case
-    True -> Prelude.putStrLn "Skipping focus (already exists)"
+    True -> putStrLn "Skipping focus (already exists)"
     False -> do
       callProcess "git"
         [ "submodule"
@@ -46,35 +43,20 @@ main = do
         , "--recursive"
         ]
   mkdirs ["common/src", "frontend/src", "backend/src", "static", "config"]
-  baseDir <- getCurrentDirectory
   let defNix    = "default.nix"
-  let main      = "Main.hs"
-  let frontend  = "frontend/src" :: FilePath
-  let backend   = "backend/src" :: FilePath
-  doesFileExist defNix >>= \case
-    True -> Prelude.putStrLn "Skipping default.nix (already exists)"
-    False -> BS.writeFile defNix $ nixExpr projectName
-  --TODO: C'mon... be more clever with this. Find a good function.
-  setCurrentDirectory frontend
-  doesFileExist main >>= \case
-    True -> Prelude.putStrLn "Skipping frontend/src/Main.hs (already exist)"
-    False -> Prelude.writeFile main frontSrc
-  setCurrentDirectory baseDir
-  setCurrentDirectory backend
-  doesFileExist main >>= \case
-    True -> Prelude.putStrLn "Skipping frontend/src/Main.hs (already exist)"
-    False -> Prelude.writeFile main backSrc
+  let frontend  = "frontend/src/Main.hs" :: FilePath
+  let backend   = "backend/src/Main.hs" :: FilePath
+  -- Warning: Data.FileEmbed (embedFile functions) may not work with all environments.
+  createFileIfMissing defNix $(embedStringFile "ob-init-default.nix")
+  createFileIfMissing frontend frontSrc
+  createFileIfMissing backend backSrc
 
-
+{-
 --TODO: Consider using fileEmbed to generate default.nix in initializing folder
-nixExpr :: ByteString -- ^ The name of the project; this must be a valid Cabal package name
-        -> ByteString
-nixExpr projectName = BS.concat
-  [ "{}: (import ./focus {}).mkDerivation { name = \""
-  , projectName
-  , "\"; version = \"0.1\"; commonDepends = p: with p; [ data-default file-embed ]; frontendDepends = p: with p; [ data-default file-embed focus-http-th focus-js ghcjs-dom reflex reflex-dom these ]; backendDepends = p: with p; [ data-default resource-pool snap snap-core snap-loader-static snap-server ];}"
-  ]
-
+nixExpr :: String -- ^ The name of the project; this must be a valid Cabal package name
+        -> String
+nixExpr projectName = USE UNLINES TO MERGE PROJECT NAME WITH default.nix code
+-}
 frontSrc :: String
 frontSrc = "{-# LANGUAGE OverloadedStrings #-}\n\nimport Reflex.Dom\n\nmain = mainWidget $ text \"Hello, new project!\"" 
 
@@ -84,3 +66,9 @@ backSrc = "{-# LANGUAGE OverloadedStrings #-}\n\nimport Data.Default\nimport Foc
 
 mkdirs :: [FilePath] -> IO ()
 mkdirs = mapM_ $ createDirectoryIfMissing True
+
+createFileIfMissing :: FilePath -> String -> IO ()
+createFileIfMissing aFile content = 
+  doesFileExist aFile >>= \case
+    True -> putStrLn "Skipping frontend/src/Main.hs (already exist)"
+    False -> writeFile aFile content
