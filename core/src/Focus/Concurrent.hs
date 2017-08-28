a181da96130fcd61b96edaed520354bd7874f861
Ali Abrar <aliabrar@gmail.com>
2017-08-28 18:06:38 -0400

    Add delay between supervise restarts

--- core/src/Focus/Concurrent.hs
+++ core/src/Focus/Concurrent.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 module Focus.Concurrent where
 
+import Control.Concurrent
 import Control.Concurrent.Async
 import Control.Exception
 import Control.Monad
@@ -11,6 +12,7 @@ supervise :: Show a => IO a -> IO ()
 supervise a = forever $ withAsync a $ \child -> do
   result <- waitCatch child
   printResult :: Either SomeException () <- try $ putStrLn $ "supervise: child terminated with " <> show result <> "; restarting"
+  threadDelay 1000000
   when (isLeft printResult) $ putStrLn "supervise: note: an exception was encountered when printing the previous result"
 
 withLinkedThread :: IO () -> IO a -> IO a
