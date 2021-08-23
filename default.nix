ddca2e1f2d537462112157691ce4ce8cef6ebeb4
Rune K. Svendsen <runesvend@gmail.com>
2021-08-23 12:12:30 -0400

    prevent Nix from trying to provide 'log' package

--- default.nix
+++ default.nix
@@ -6,7 +6,10 @@ let
       let baseName = baseNameOf path; in baseName != ".git"
     )
     ./.;
-  pkg = callCabal2nix "android-activity" src {};
+  pkg = callCabal2nix "android-activity" src {
+    # prevent Nix from trying to provide the "log" package
+    log = null;
+  };
 in
   pkgs.haskell.lib.overrideCabal pkg (drv: {
     librarySystemDepends = (drv.librarySystemDepends or []) ++ [jdk];
